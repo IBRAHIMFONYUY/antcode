9d48c1ba7a7b38d5f6bdb66a0cf79855a14dd183
Ibrahim Fonyuy <ibrahimfonyuy06@gmail.com>
2025-12-15 16:16:18 +0000

    Runtime FirebaseError
    Failed to get document because the client is off

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,6 @@
 import { getApps, initializeApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
-import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getFirestore, type Firestore, enableIndexedDbPersistence } from 'firebase/firestore';
 import { firebaseConfig } from './config';
 
 // The infamous "Firebase has already been initialized" error is avoided
@@ -16,6 +16,7 @@ function initializeFirebase() {
 
 // We store the instances in a memoized object to avoid re-creating them on every render.
 const services = new Map();
+let persistenceEnabled = false;
 
 function getFirebaseServices() {
   const app = initializeFirebase();
@@ -25,6 +26,18 @@ function getFirebaseServices() {
 
   const auth = getAuth(app);
   const firestore = getFirestore(app);
+
+  if (!persistenceEnabled) {
+    enableIndexedDbPersistence(firestore).catch((err) => {
+      if (err.code == 'failed-precondition') {
+        console.warn('Firestore persistence failed: Multiple tabs open');
+      } else if (err.code == 'unimplemented') {
+        console.warn('Firestore persistence failed: Browser does not support it.');
+      }
+    });
+    persistenceEnabled = true;
+  }
+  
 
   const newServices = { app, auth, firestore };
   services.set(app, newServices);
